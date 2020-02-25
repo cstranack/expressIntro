7ff3f0ba8fdc13c17e8482135faf0c162f04bd64
Claire Stranack <claire@stranack.co.uk>
2020-02-25 12:10:00 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+//web application getting something from the server
+//req = request //res = response //=>'fat arrow' = function
+app.get('/', (req, res) => {
+    res.send('This is the homepage :D ');
+
+});
+
+app.get('/about', (req, res) => {
+    res.send('This is the about page :D ');
+
+});
+
+
+
+
+//listening for requests on port 3000
+app.listen(3000,() => {
+    console.log(' Server listening on port 3000 :) ')
+})
